8eb6b10b86a0bfbf8239244f83b89c52c575e43e
Rosanna <rsnzhang@gmail.com>
2016-06-26 20:27:42 -0400

    Editted version 1 of projectEuler3

--- projectEuler3.py
+++ projectEuler3.py
@@ -1,33 +1,26 @@
 #Problem 3
-
 #The prime factors of 13195 are 5, 7, 13 and 29.
-
 #What is the largest prime factor of the number 600851475143 ?
-
-number = int(raw_input('Please Enter an Integer: '))
-factor = 2
-smallest = int()
-largest = int()
-
-def isPrime(x):
-    global largest
-    for i in range(2,int(x**(0.5)+1)):
-        if x%i==0:
-#           if isPrime(i)==True:
-#                largest = i
-            return False
-    return True
+def main():
+    number = int(raw_input('Please Enter an Integer: '))
+    factor = 2
     
-if isPrime(number)==True:
-    largest = number
-    
-else:    
-    while number >1:
-#        if number%factor == 0 and isPrime(factor)==True:       
-#            smallest = factor
-        if number%factor == 0 and isPrime(number/factor)==True:
-            largest = number/factor
-            break
-        factor+=1
-            
-print 'The largest prime factor of ' + str(number) + ' is ' + str(largest) + '.'
+    def isPrime(x):
+        global largest
+        for i in range(2,int(x ** (0.5) + 1)):
+            if x%i == 0:
+                return False
+        return True    
+        
+    if isPrime(number) == True:
+        largest = number
+    else:    
+        while number > 1:
+            if number % factor == 0 and isPrime(number / factor) == True:
+                largest = number/factor
+                break
+            factor+=1           
+    print 'The largest prime factor of ' + str(number) + ' is ' + str(largest) + '.'
+
+if __name__ == '__main__':
+    main()
