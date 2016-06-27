e715aa7e23720bc975755d77d4a363649906b4ad
Rosanna <rsnzhang@gmail.com>
2016-06-26 20:51:34 -0400

    modified version2

--- projectEuler3.py
+++ projectEuler3.py
@@ -3,15 +3,10 @@
 #What is the largest prime factor of the number 600851475143 ?
 def main():
     number = int(raw_input('Please Enter an Integer: '))
+    print 'The largest prime factor of ' + str(number) + ' is ' + str(findLargestPrimeFactor(number)) + '.'
+
+def findLargestPrimeFactor(number):  
     factor = 2
-    
-    def isPrime(x):
-        global largest
-        for i in range(2,int(x ** (0.5) + 1)):
-            if x%i == 0:
-                return False
-        return True    
-        
     if isPrime(number) == True:
         largest = number
     else:    
@@ -19,8 +14,15 @@ def main():
             if number % factor == 0 and isPrime(number / factor) == True:
                 largest = number/factor
                 break
-            factor+=1           
-    print 'The largest prime factor of ' + str(number) + ' is ' + str(largest) + '.'
+            factor+=1
+    return largest
 
+def isPrime(x):
+        global largest
+        for i in range(2,int(x**(0.5) + 1)):
+            if x % i == 0:
+                return False
+        return True  
+        
 if __name__ == '__main__':
     main()
